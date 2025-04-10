c05b93d9f4f584b0eac8d67bf324e7ac775c07c4
anilatitus99 <anilatitus99@gmail.com>
2025-04-10 19:20:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_youtube():
-    target = os.environ.get('TARGET', 'Youtube you are now on KUBERNETES !!')
+    target = os.environ.get('TARGET', 'Youtube you are now on Google Cloud !!')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
