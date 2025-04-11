6bf4c805750da05bf78941264322220ca1c44ca4
anilatitus99 <anilatitus99@gmail.com>
2025-04-10 20:33:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-
-import os
-
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_youtube():
-    target = os.environ.get('TARGET', 'Youtube you are now on KUBERNETES !!')
-    return 'Hello {}!\n'.format(target)
+def hello_world():
+    return 'Hello, YouTube!'
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+if __name__ == '__main__':
+    # Run the application on the default port 8080 (which is used in Kubernetes containers)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
